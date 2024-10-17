40fcc0ddb1be46d3170408bfed0aa5388eb96a22
Thupten Wangpo <110739214+Dodhon@users.noreply.github.com>
2024-10-17 17:27:15 -0500

    part b done

--- Wangpo_Thupten/cs481_P02B_A20512003.py
+++ Wangpo_Thupten/cs481_P02B_A20512003.py
@@ -13,13 +13,11 @@ S = input("Please enter a sentence S from a keyboard: ")
 # Apply lowercasing to S.
 S = S.lower()
 
-#[45 pts] Calculate P(S) assuming a 2-gram language model (assume that probability of any bigram starting or ending a sentence is 0.25)
+#Calculate P(S) assuming a 2-gram language model (assume that probability of any bigram starting or ending a sentence is 0.25)
 
 
 nltk.download('brown')
 
-def calculateProbability(count, total):
-    return count / total if total > 0 else 0
 
 brown_words = brown.words()
 brown_words = [word.lower() for word in brown_words]
@@ -30,8 +28,42 @@ for word in brown_words:
 tokensOfS = word_tokenize(S)
 unigramFreq = dict()
 unigrams_brown = sum(frequencyDistributionBrown.values())
-for unigram in tokensOfS:
-    count = frequencyDistributionBrown.get(unigram, 0)
-    probability = calculateProbability(count, unigrams_brown) 
-    unigramFreq[unigram] = probability
-print(unigramFreq)
+#print(f"brownFrequenciesAndWords is {brownFrequenciesAndWords}")
+
+N = 2
+bigramsInS = nltk.ngrams(S.split(), N)
+bigramListS = [bigram for bigram in bigramsInS]
+bigramsInBrown = nltk.ngrams(brown_words, N)
+brownBigramList = [bigram for bigram in bigramsInBrown]
+# print(f"In the brown corpus, there are {len(brownBigramList)} bigrams.")
+bigramFreq = dict()
+for bigram in brownBigramList: ## stopwords are not to be removed
+    if bigram in bigramFreq:
+        bigramFreq[bigram] += 1
+    else:
+        bigramFreq[bigram] = 1
+first = tokensOfS[0]
+last = tokensOfS.pop()
+bigramProb = dict()
+bigramProb[f"Probability of ('<s>' ,'{first}) is'"] = .25
+for unigram, bigram in zip(tokensOfS, bigramListS):
+    if bigram not in bigramFreq or unigram not in brownFrequenciesAndWords:
+        bigramProb[f"Probability of {bigram}"] = 0
+    else:
+        bigramProb[f"Probability of {bigram} is"] = bigramFreq[bigram]/brownFrequenciesAndWords[unigram]
+bigramProb[f"Probability of ('{last}','</s>') is"] = .25
+
+
+# Display the sentence S, list all the individual bigrams and their probabilities, and the final probability P(S) on screen. It is fine if it is zero.
+print("___________________________")
+print(f"The sentence S is: '{S}'")
+for i,v in zip(bigramProb.keys(),bigramProb.values()):
+    print(f"{i} : {v}")
+finalProb = 1
+for prob in bigramProb.values():
+    finalProb = finalProb*prob
+print(f"The final probability P(S) is {finalProb}")
+
+    
+    
+    
